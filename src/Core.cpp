67ea9f7689d2f3a28eed5ceaf5e97ce50fe17b6a
Thibault FOUCOU <thibault.foucou@epitech.eu>
2019-05-28 17:55:35 +0200

    put new button

--- src/Core.cpp
+++ src/Core.cpp
@@ -12,6 +12,7 @@ Core::Core(IrrlichtDevice *const window) : _window(window)
     this->driver = window->getVideoDriver();
     this->image = this->driver->getTexture("./resources/background.jpg");
     this->button1 = this->driver->getTexture("./resources/EXIT.png");
+    this->button2 = this->driver->getTexture("./resources/PLAY.png");
     this->smgr = window->getSceneManager();
     this->env = window->getGUIEnvironment();
 	initMenu();
@@ -27,10 +28,10 @@ void Core::run()
 
     while (_window->run()) {
         button_quit->setImage(this->button1);
-        button_quit->setRelativePosition(position2d<s32>(900, 400));
+        button_quit->setRelativePosition(position2d<s32>(748, 800));
         this->driver->beginScene(true, true, SColor(255, 100, 101, 140));
         this->driver->draw2DImage(this->image, position2d<s32>(0, 0), rect<s32>(0, 0, 1920, 1080), 0, SColor(255, 255, 255, 255), true);
-        //this->driver->draw2DImage(this->button1, position2d<s32>(900, 400), rect<s32>(0, 0, 425, 77), 0, SColor(255, 255, 255, 255), true);
+        this->driver->draw2DImage(this->button2, position2d<s32>(748, 200), rect<s32>(0, 0, 425, 77), 0, SColor(255, 255, 255, 255), true);
         this->smgr->drawAll();
         this->env->drawAll();
         this->driver->endScene();
