204eab0c4e6ace1e9153ca82a666c579b625b67b
Tristan Morera <tristan.morera@epitech.eu>
2019-06-10 17:35:26 +0200

    menu buttons

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -10,15 +10,15 @@
 void Menu::loadTextures(Graphic *graphic)
 {
     this->_textures.insert(std::pair<std::string, irr::video::ITexture *>(std::string("menuBackground"),
-            graphic->getDriver()->getTexture("./ressources/background.jpg")));
+            graphic->getDriver()->getTexture("./assets/images/background.png")));
     this->_textures.insert(std::pair<std::string, irr::video::ITexture *>(std::string("menuExitButton"),
-            graphic->getDriver()->getTexture("./ressources/EXIT.png")));
+            graphic->getDriver()->getTexture("./assets/buttons/leave.png")));
     this->_textures.insert(std::pair<std::string, irr::video::ITexture *>(std::string("menuPlayButton"),
-            graphic->getDriver()->getTexture("./ressources/PLAY.png")));
+            graphic->getDriver()->getTexture("./assets/buttons/play.png")));
     this->_textures.insert(std::pair<std::string, irr::video::ITexture *>(std::string("menuHelpButton"),
-            graphic->getDriver()->getTexture("./ressources/HELP.png")));
+            graphic->getDriver()->getTexture("./assets/HELP.png")));
     this->_textures.insert(std::pair<std::string, irr::video::ITexture *>(std::string("menuOptionsButton"),
-            graphic->getDriver()->getTexture("./ressources/OPTIONS.png")));
+            graphic->getDriver()->getTexture("./assets/buttons/options.png")));
     this->_textures.insert(std::pair<std::string, irr::video::ITexture *>(std::string("menuTestButton"),
             graphic->getDriver()->getTexture("./ressources/test.jpg")));
 }
@@ -26,17 +26,17 @@ void Menu::loadTextures(Graphic *graphic)
 void Menu::loadButtons(Graphic *graphic)
 {
     this->_buttons.insert(std::pair<std::string, irr::gui::IGUIButton *>(std::string("menuExit"),
-            graphic->getEnv()->addButton(irr::core::rect<irr::s32>(0, 0, 425, 77), nullptr, GUI_ID_QUIT_BUTTON, L"Exit", L"Exits Program")));
+            graphic->getEnv()->addButton(irr::core::rect<irr::s32>(0, 0, 215, 47), nullptr, GUI_ID_QUIT_BUTTON, L"Exit", L"Exits Program")));
     this->_buttons.insert(std::pair<std::string, irr::gui::IGUIButton *>(std::string("menuPlay"),
-            graphic->getEnv()->addButton(irr::core::rect<irr::s32>(0, 0, 425, 77), nullptr, GUI_ID_PLAY_BUTTON, L"Play", L"Play game")));
+            graphic->getEnv()->addButton(irr::core::rect<irr::s32>(0, 0, 215, 47), nullptr, GUI_ID_PLAY_BUTTON, L"Play", L"Play game")));
     this->_buttons.insert(std::pair<std::string, irr::gui::IGUIButton *>(std::string("menuOptions"),
-            graphic->getEnv()->addButton(irr::core::rect<irr::s32>(0, 0, 425, 77), nullptr, GUI_ID_OPTIONS_BUTTON, L"", L"Set options")));
+            graphic->getEnv()->addButton(irr::core::rect<irr::s32>(0, 0, 215, 47), nullptr, GUI_ID_OPTIONS_BUTTON, L"", L"Set options")));
     this->_buttons["menuExit"]->setImage(this->_textures["menuExitButton"]);
     this->_buttons["menuPlay"]->setImage(this->_textures["menuPlayButton"]);
     this->_buttons["menuOptions"]->setImage(this->_textures["menuOptionsButton"]);
-    this->_buttons["menuExit"]->setRelativePosition(irr::core::position2d<irr::s32>(748, 800));
-    this->_buttons["menuPlay"]->setRelativePosition(irr::core::position2d<irr::s32>(748, 200));
-    this->_buttons["menuOptions"]->setRelativePosition(irr::core::position2d<irr::s32>(748, 500));
+    this->_buttons["menuExit"]->setRelativePosition(irr::core::position2d<irr::s32>(850, 800));
+    this->_buttons["menuPlay"]->setRelativePosition(irr::core::position2d<irr::s32>(300, 500));
+    this->_buttons["menuOptions"]->setRelativePosition(irr::core::position2d<irr::s32>(1400, 500));
 }
 
 Menu::Menu(Graphic *graphic)
