4fe178c2943004c9353c3101de81d751ca9fc7f1
ROUVIER <til.rouvier@epitech.eu>
2019-06-11 15:33:12 +0200

    read map and stock in vector

--- src/map/read_map.cpp
+++ src/map/read_map.cpp
@@ -5,12 +5,37 @@
 // Indie Studio
 //
 
+#include <iostream>
+#include <fstream>
+#include <string>
 #include <vector>
 
-int read_map(int ac, char **av)
+bool getFileContent(std::string fileName, std::vector<std::string> &vecOfStrs)
 {
-    FILE* file = NULL;
-    file = fopen("map.txt", "r+");
 
+    std::ifstream in(fileName.c_str());
 
+    if (!in) {
+        std::cerr << "Loading map failed: "<<fileName<<std::endl;
+        return false;
+    }
+
+    std::string str;
+    while (std::getline(in, str)) {
+        if(str.size() > 0)
+            vecOfStrs.push_back(str);
+    }
+    in.close();
+    return true;
+}
+
+int read_map()
+{
+    std::vector<std::string> vecOfStr;
+    bool result = getFileContent("map.txt", vecOfStr);
+
+    if(result) {
+        for(std::string &line:vecOfStr)
+            std::cout<<line<<std::endl;
+    }
 }
